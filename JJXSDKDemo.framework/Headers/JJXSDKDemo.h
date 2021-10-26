e801f1149683ab6d7fa0588629b5d906316e0c70
huangju6 <huangju6@jd.com>
2021-10-26 15:17:41 +0800

    init

---
+++ JJXSDKDemo.framework/Headers/JJXSDKDemo.h
@@ -0,0 +1,19 @@
+//
+//  JJXSDKDemo.h
+//  JJXSDKDemo
+//
+//  Created by huangju6 on 2021/10/15.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for JJXSDKDemo.
+FOUNDATION_EXPORT double JJXSDKDemoVersionNumber;
+
+//! Project version string for JJXSDKDemo.
+FOUNDATION_EXPORT const unsigned char JJXSDKDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JJXSDKDemo/PublicHeader.h>
+
+
